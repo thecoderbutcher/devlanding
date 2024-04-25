fb3a4d200a21bb40e3d4223347290cbf155083bc
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2024-04-25 11:14:36 -0300

    feat: feactures section | fixed build code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Feature from './components/Feature'
 
 const App = () => {
   return (
     <>
       <Navbar/>
-      <Hero/>
+      <div className='max-w-7xl mx-auto pt-20 px-6'>
+        <Hero/>
+        <Feature/>
+      </div>
     </>
   )
 }
